d96114d062bdd329bf2fce8216b81fff89997188
Michael Wechner <michael.wechner@wyona.com>
2022-11-21 16:48:56 +0100

    source added

--- discord_bot.py
+++ discord_bot.py
@@ -50,8 +50,10 @@ class MyClient(discord.Client):
         bot.set_chat_history(history)
 
         async with message.channel.typing():
-            reply = bot.answer(message.clean_content, verbosity=2, n_paragraphs=3)
-            response_msg = await message.channel.send(reply, reference=message)
+            reply, source_urls, source_texts = bot.answer(message.clean_content, verbosity=2, n_paragraphs=3)
+            sources_str = "\n".join(list(set(source_urls)))
+            reply_incl_sources=f"{reply}\nSource:\n{sources_str}"
+            response_msg = await message.channel.send(reply_incl_sources, reference=message)
             await response_msg.edit(suppress=True)
             return
 
